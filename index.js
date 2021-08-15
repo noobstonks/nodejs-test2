e036b60466384d1ba01e962c857f4e938611e162
noobstonks <androfarlinno@gmail.com>
2021-08-15 13:41:23 +0700

    finish http test server

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require ("http");
+// const fs = require ("fs");
+
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"})
+//     fs.readFile("./index.html", null, (error,data) => {
+//         if(error) {
+//             response.writeHead(404);
+//             response.write("file not found")
+//         }
+//         else{
+//             response.write(data)
+//         }
+//         response.end()
+//     })
+// }
+
+// http.createServer(onRequest).listen(8000);
+
+function onRequest (request, response){
+    response.writeHead(200, {"Content-Type": "application/json"})
+    const data = {
+        name: "Yudi Krisnandi",
+        age: 22
+    }
+
+    response.end (JSON.stringify(data))
+}
+
+http.createServer(onRequest).listen(8000)
